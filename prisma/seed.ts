9a379ef311eb92d551b4300aaaf3931149bad376
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2026-01-07 09:09:58 +0700

    feat(smartax): remove delete many

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,10 +2,6 @@ import { prisma } from "@/lib/prisma";
 import data from './seminar.seed.json';
 
 async function main() {
-  
-  await prisma.$executeRawUnsafe(
-  `TRUNCATE TABLE Seminar`
-)
 
   await prisma.seminar.createMany({
     data
